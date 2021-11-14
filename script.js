a2af6a1a6b045cb040e8d91763b72b65fc53142c
Don Kim <dkim05@gmail.com>
2021-11-13 21:49:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$( "#nav-placeholder" ).load( "content_a.html", function( response, status, xhr ) {
+$( "#content_a" ).load( "content_a.html", function( response, status, xhr ) {
   if ( status == "error" ) {
     var msg = "Sorry but there was an error: ";
     $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
