3f529834964f614438d865b932720f53e7334135
Don Kim <dkim05@gmail.com>
2021-11-13 21:48:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$( "#nav-placeholder" ).load( "https://insightdon.github.io/prototype_1/content_a.html", function( response, status, xhr ) {
+$( "#nav-placeholder" ).load( "content_a.html", function( response, status, xhr ) {
   if ( status == "error" ) {
     var msg = "Sorry but there was an error: ";
     $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
