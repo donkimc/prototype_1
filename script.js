f52aa5a8d621ef1471b0b9c4fd9edfbc557895b7
Don Kim <dkim05@gmail.com>
2021-11-13 21:43:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+$( "#nav-placeholder" ).load( "https://insightdon.github.io/prototype_1/content_a.html", function( response, status, xhr ) {
+  if ( status == "error" ) {
+    var msg = "Sorry but there was an error: ";
+    $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
+  }
+});
